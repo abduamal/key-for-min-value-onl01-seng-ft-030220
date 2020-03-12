5a1bb4668b03ff4408a2c5e1799a5fdf149747c0
Amal Abdu <abduamal@ymail.com>
2020-03-12 04:19:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  i = 0
+  small = []
+  name_hash.each do |word, num|
+    if name_hash == nil
+      return nil
+    elsif num < name_hash[1][i]
+      small << word
+      i += 1
+    end
+  end
+  return small[0]
 end
